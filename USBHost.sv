5cd5b150e05ae9ed6c35d7a2c13aab31aa4cabe8
mmao2 <mmao2@ece001.ece.local.cmu.edu>
2025-03-22 19:39:05 -0400

    Initial NRZI and Bit stuffing modules

--- USBHost.sv
+++ USBHost.sv
@@ -5,13 +5,77 @@ typedef enum logic [1:0]
   {BS_J = 2'b10, BS_K = 2'b01, BS_SE0 = 2'b00, BS_SE1 = 2'b11, BS_NC = 2'bzz}
   bus_state_t;
 
+
+module NRZI (
+  input logic stream,
+  input logic clock, reset_n,
+  output logic out,
+);
+  always_ff @(posedge clock, negedge reset_n) begin
+    if (reset_n == 0)
+      out <= 1;
+    else begin 
+      if (stream) begin 
+        out <= ~out;
+      end
+      else begin 
+        out <= out;
+      end
+    end
+  end
+endmodule NRZI;
+
+module BitStuffer (
+  input logic stream;
+  input logic clock, reset_n,
+  output logic pause,
+  output logic out
+)
+  logic [3:0] count;
+  always_ff @(posedge clock, negedge reset_n) begin
+    if (reset_n == 0)
+      count <= 0;
+    else begin
+      if (pause == 1) begin
+        out <= 0;
+        pause <= 0;
+      end
+      else begin 
+        if (stream == 1) begin 
+          count <= count + 1;
+        end
+        else begin
+          count <= 0;
+        end
+        if (count == 6) begin 
+          pause <= 1;
+          count <= 0;
+        end
+        else begin
+          out <= steam;
+        end
+      end
+    end
+  end
+endmodule BitStuffer;
+
+
 module USBHost (
   USBWires wires,
   input logic clock, reset_n
 );
 
 task prelabRequest();
+  logic [6:0] device_address;
+  logic [3:0] data_endpoint;
+  device_address = DEVICE_ADDR;
+  data_endpoint = DATA_ENDP;
+  
 
+  logic [23:0] packet;
+  packet = 24'b1101_1111_1000_1111_1111_0011;
+  
+  
 endtask : prelabRequest
 
 task readData
